b9526488a6e4d658e67dc7dcc9d1bf0b751a266f
fuseMuskan <muskan.bhandari@fusemachines.com>
2024-01-19 14:20:57 +0545

    fix: removed unnused imports and functions

--- doc_classification_with_transfer_learning/eval.py
+++ doc_classification_with_transfer_learning/eval.py
@@ -33,37 +33,10 @@ def plot_confusion_matrix(conf_matrix, class_names):
     return fig
 
 
-def plot_to_image(fig):
-    # Saving the plot to a PNG in memory.
-    buffer = io.BytesIO()
-    fig.savefig(buffer, format="png")
-    plt.close(fig)
-    buffer.seek(0)
-    # Converting PNG buffer to PIL Image
-    pil_image = Image.open(buffer).convert("RGBA")
-
-    # Converting PIL Image to PyTorch tensor
-    image = torch.tensor(np.array(pil_image)).permute(2, 0, 1).float() / 255.0
-
-    # Adding the batch dimension
-    image = image.unsqueeze(0)
-
-    return image
-
-
 def save_confusion_matrix(conf_matrix):
     class_names = ["citizenship", "license", "others", "passport"]
 
     fig = plot_confusion_matrix(
         conf_matrix=conf_matrix.numpy(), class_names=class_names
     )
-    # conf_image = plot_to_image(fig)
-    print("PASSED..............")
     return fig
-
-
-input = torch.tensor([0, 2, 1, 3])
-target = torch.tensor([0, 1, 2, 3])
-
-conf_matrix = calculate_confusion_matrix(input, target)
-save_confusion_matrix(conf_matrix)
